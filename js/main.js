eb8a5b63f6090cade5ab3ef4f8d05529e1d1de78
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2016-05-30 16:14:04 -0400

    new home

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
 $(document).ready( function() {
-  $('button').on('click', function() {
-    function button_display(a,b,c) {
-      if( !(a === '' && b === '' && c === '') ) {
-        // $('body').scrollTo('#tools');
-        $('html, body').animate({
-          scrollTop: $('#tools').offset().top
-        }, 0);
-      }
 
-      $('.category_01').parent().parent().css('display',a);
-      $('.category_02').parent().parent().css('display',b);
-      $('.category_03').parent().parent().css('display',c);
-    }
-
-    if( $(this).data('category') === 'category_01' && $(this).data('show') === true ) {
-      button_display('','none','none');
-      $(this).html('Limpiar filtro');
-      $(this).data('show',false);
-    } else if( $(this).data('category') === 'category_02' && $(this).data('show') === true ) {
-      button_display('none','none','');
-      $(this).html('Limpiar filtro');
-      $(this).data('show',false);
-    } else if( $(this).data('category') === 'category_03' && $(this).data('show') === true ) {
-      button_display('none','','none');
-      $(this).html('Limpiar filtro');
-      $(this).data('show',false);
-    } else {
-      button_display('','','');
-      $('#three-axis button').html('Conócelas aquí');
-      $('#three-axis button').data('show',true);
-    }
-
-  })
 });
